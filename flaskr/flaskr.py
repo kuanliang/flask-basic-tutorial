18c09933f2cfa73c23b08798c7370ec52044218c
KuanLiang <kliangliu@gmail.com>
2016-07-21 12:47:22 +0800

    typo fixed

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -30,7 +30,7 @@ FLASKR_SETTINGS = './config.py'
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-#
+# initialize the database
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
